b208f67f9eab2b2ee7a3188140a524195aa2d768
KhankhasaevRV <rv2100@mail.ru>
2019-01-24 02:17:24 +0300

    Homework4

--- src/ru/innopolis/Homework4/Main.java
+++ src/ru/innopolis/Homework4/Main.java
@@ -1,110 +1,153 @@
 package ru.innopolis.Homework4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.concurrent.*;
 
 public class Main {
-    public static final int COUNT_SOURCES = 20;
-    public static final int COUNT_WORDS = 100;
+    private static final int COUNT_SOURCES = 50;
+    private static final int COUNT_THREAD = 10;
 
-//    static String[] sources = new String[COUNT_SOURCES];
-    static String[] words = {"NOT", "AND", "MIKE"};
+    private static String[] words = {"ALICE", "WORKING", "MIKE"};
 
-
-    static String path = "out\\result.txt";
+    private static String path = "./out/result.txt";
 
 
+    public static void main(String[] args) {
 
+        String[] sources = new String[COUNT_SOURCES];
 
-    public static void main(String[] args) throws FileNotFoundException, ExecutionException, InterruptedException {
-
-        String[] sources = new String[10];
-
-
-         for (int i = 0; i < 10; i++) {
-//            sources[i] = "file:out\\file_" +(i+1)+".txt";
-             sources[i] = "http://www.gutenberg.org/ratelimiter.php/cache/epub/" + (i+1) +"/pg" + (i+1) +".txt";
-         }
-
-//        for (int i = 10; i < sources.length; i++) {
-//            //int id = ThreadLocalRandom.current().nextInt(50000);
-////            sources[i] = "http://gutenberg.org/cache/epub/" + i + "/pg" + i + ".txt";
-//            sources[i] = "http://www.gutenberg.org/ratelimiter.php/cache/epub/" + i +"/pg" +i +".txt";
-//        }
-
-
-        List<ThreadSource> listThreadSource = new ArrayList<>();
-        List<FutureTask<List<String>>> listFutureTask = new ArrayList<>();
-
-
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        List<Future<List<String>>> listFuture = new ArrayList<Future<List<String>>>(); // сюда результаты
-
-        for (int i = 0; i < sources.length; i++) {
-            listThreadSource.add(new ThreadSource(sources[i], words));
-            listFutureTask.add(new FutureTask<List<String>>(listThreadSource.get(i)));
-            //executorService.execute(listFutureTask.get(i));
+        for (int i = 0; i < COUNT_SOURCES; i++) {
+            sources[i] = "http://www.gutenberg.org/ratelimiter.php/cache/epub/" + (i + 1) + "/pg" + (i + 1) + ".txt";
         }
 
-//        for(int i=0; i< sources.length; i++){
-//            //сабмитим Callable таски, которые будут
-//            //выполнены пулом потоков
-//            Future<List<String>> future = executorService.submit(listThreadSource.get(i));
-//            //добавляя Future в список,
-//            //мы сможем получить результат выполнения
-//            listFuture.add(future);
-//        }
+        try {
+            long timeBegin = System.currentTimeMillis();
+            getOccurencies(sources, words, path);
+            long timeEnd = System.currentTimeMillis();
+            Date date = new Date(timeEnd - timeBegin);
+            DateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
+            formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+            String dateFormatted = formatter.format(date);
+            System.out.println("Количество потоков - " + COUNT_THREAD + ", Количество ресурсов - " + COUNT_SOURCES + " время - " + dateFormatted);
+        } catch (FileNotFoundException e) {
+            System.out.println("Не найден файл");
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            System.out.println("Что то пошло не так");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("Поток был прерван");
+            e.printStackTrace();
+        }
+    }
 
-        for (int i = 0; i < 5; i++) {
+    /**
+     * Проверяет все ли потоки завершены
+     *
+     * @param listFutureTask лист потоков
+     * @return true если все потоки завершены, иначе false
+     */
+
+    private static boolean allTasksDone(List<FutureTask<List<String>>> listFutureTask) {
+        boolean isDone = true;
+        for (int i = 0; i < COUNT_THREAD; i++) {
+            if (!listFutureTask.get(i).isDone()) {
+                isDone = false;
+                break;
+            }
+        }
+        return isDone;
+    }
+
+
+    /**
+     * Ищет количество вхождений слов из words в ресурсах sources
+     *
+     * @param sources ресурсы
+     * @param words   искомые слова
+     * @param res     результиющий файл, куда записывают предложения со словами
+     * @throws FileNotFoundException - если
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private static void getOccurencies(String[] sources, String[] words, String res) throws FileNotFoundException, ExecutionException, InterruptedException {
+
+        if (!clearResultFile(path)){
+            System.out.println("Не могу удалить результирующий файл");
+            return;
+        }
+        List<ThreadSource> listThreadSource = new ArrayList<>();
+        List<FutureTask<List<String>>> listFutureTask = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(COUNT_THREAD);
+
+        for (int i = 0; i < COUNT_SOURCES; i++) {
+            listThreadSource.add(new ThreadSource(sources[i], words));
+            listFutureTask.add(new FutureTask<List<String>>(listThreadSource.get(i)));
             executorService.execute(listFutureTask.get(i));
         }
 
 
-        List<String > resultSentence = new ArrayList<>();
-
         while (true) {
-            try {
-                if(listFutureTask.get(0).isDone() &&
-                        listFutureTask.get(1).isDone() &&
-                        listFutureTask.get(2).isDone() &&
-                        listFutureTask.get(3).isDone() &&
-                        listFutureTask.get(4).isDone()){
-                    System.out.println("Done");
-                    // заканчиваем работу executor service
-                    executorService.shutdown();
-                    for(FutureTask el: listFutureTask) {
-                        List<String> dd = (List<String>) el.get();
-
-                        System.out.println(el.get());
-                    }
-                    return;
-                }
+            if (allTasksDone(listFutureTask)) {
+                System.out.println("Done");
+                executorService.shutdown();
+                break;
             }
-            finally {
-
-            }
-
-
-//            catch (InterruptedException e) {
-//                e.printStackTrace();
-//            } catch (ExecutionException e) {
-//                e.printStackTrace();
-//            }
         }
 
-//        for(Future el: listFuture) {
-//            System.out.println(el.get());
-//        }
-
-
-
-
+        clearResultFile(path);
+        for (FutureTask el : listFutureTask) {
+            List<String> sourceSentenceList = (List<String>) el.get();
+            writeToFile(sourceSentenceList, path);
+            System.out.println(sourceSentenceList.size());
+        }
     }
 
-    void getOccurencies(String[] sources, String[] words, String res) {
 
+    /**
+     * Очищает файл path
+     * @param path путь к файлу для очистки
+     * @return true если файл удален, иначе false
+     */
+    static boolean clearResultFile(String path)  {
+
+        File fileobj = new File(path);
+        if (fileobj.exists()) {
+            return fileobj.delete();
+        }
+        return false;
+    }
+
+    /**
+     * Записывает в файл path из List<String> sentenceWithWords
+     *
+     * @param sentenceWithWords List<String> который необходимо записать
+     * @param path              путь к файлу, в который надо записать
+     * @throws FileNotFoundException если путь не найден или файл занят
+     */
+    private static void writeToFile(List<String> sentenceWithWords, String path) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(path, true)) {
+
+            for (String el : sentenceWithWords) {
+                byte[] buffer = (el + "\n").getBytes();
+                fileOutputStream.write(buffer, 0, buffer.length);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Путь не найден");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Что то пошло не так при записи в файл, возможно файл занят");
+            e.printStackTrace();
+        }
     }
 
 
--- src/ru/innopolis/Homework4/ThreadSource.java
+++ src/ru/innopolis/Homework4/ThreadSource.java
@@ -2,6 +2,7 @@ package ru.innopolis.Homework4;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -10,56 +11,43 @@ import java.util.regex.Pattern;
 
 public class ThreadSource implements Callable<List<String>> {
 
-    String source;
-    String[] words;
+    private String source;
+    private String[] words;
 
 
-    ThreadSource(String source, String[] words){
+    ThreadSource(String source, String[] words) {
         this.source = source;
         this.words = words;
     }
 
     @Override
-    public List<String> call() throws Exception{
+    public List<String> call() {
 
         List<String> sentenceWithWords = new ArrayList<>();
-
-//        try(InputStream input = new URL("file:out\\file_4.txt").openStream();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));) {
-
-            try(InputStream input = new URL(source).openStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));) {
-
+        try (InputStream input = new URL(source).openStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))) {
             String beginSentence = "";
 
             while (reader.ready()) {
-                String line = beginSentence + reader.readLine();
+                String nextLine = reader.readLine();
+                if (nextLine.trim().equals("") || !hasWords(nextLine)) {
+                    continue;
+                }
+                String line = beginSentence + nextLine;
                 int lastInd = findEndLastSentence(line);
                 if (lastInd == 0) {
-                    beginSentence = beginSentence + " " + line;
+                    beginSentence = beginSentence + " " + nextLine;
                     continue;
                 } else {
                     beginSentence = "";
                 }
-                // если последний значащий символ .?! значит тут нет предложения, которое началось на этой строке и закочилось на следующих
-//                ищем последнее вхождение окончания предложения в строку
-                // String dd = line.substring(findEndLastSentence(line));
-
                 findInString(line, words, sentenceWithWords);
-
-
-//                String beginSentence = null;
                 if ((lastInd != 0) && (lastInd < line.length()) && (isNotEndedSentence(line.substring(lastInd)))) {
                     beginSentence = line.substring(lastInd);
                 }
-
             }
-
-
         } catch (IOException e) {
-            System.out.println("Не могу получить данные с ресурса");
-            e.printStackTrace();
-            throw new IOException();
+            System.out.println("Не могу получить данные с ресурса - " + source);
         }
         return sentenceWithWords;
     }
@@ -68,14 +56,15 @@ public class ThreadSource implements Callable<List<String>> {
     /**
      * Заполняет List<String> findSentences предложениями из строки string, в
      * которых найдены слова из массива words
-     * @param string строка, в которой производится поиск
-     * @param words массив слов, которые необходимо искать string
+     *
+     * @param string        строка, в которой производится поиск
+     * @param words         массив слов, которые необходимо искать string
      * @param findSentences List<String> который заполняем предложениями
      */
     private void findInString(String string, String[] words, List<String> findSentences) {
         String stringUpper = string.toUpperCase();
 
-        for (int i = 0; i <words.length  ; i++) {
+        for (int i = 0; i < words.length; i++) {
             if (stringUpper.contains(words[i].toUpperCase())) {
 
                 Pattern patternWord = Pattern.compile(words[i].toUpperCase());
@@ -85,7 +74,7 @@ public class ThreadSource implements Callable<List<String>> {
                     Matcher matcherSentence = patternSentence.matcher(string);
                     while (matcherSentence.find()) {
                         if (matcherWord.start() >= matcherSentence.start() && matcherWord.end() <= matcherSentence.end()
-                                && (! findSentences.contains(string.substring(matcherSentence.start(), matcherSentence.end())))) {
+                                && (!findSentences.contains(string.substring(matcherSentence.start(), matcherSentence.end())))) {
 
                             findSentences.add(string.substring(matcherSentence.start(), matcherSentence.end()));
                         }
@@ -96,24 +85,26 @@ public class ThreadSource implements Callable<List<String>> {
     }
 
 
-    /** Возвращает индекс конца последнего предложения в строке
+    /**
+     * Возвращает индекс конца последнего предложения в строке
      * Необходим если начало одного предложения находится на одной строке, а конец на другой.
      *
      * @param line анализируемая строка
      * @return индекс последнего конца предложения или 0 если нет окончания предложения
      */
 
-    private static int  findEndLastSentence(String line){
+    private int findEndLastSentence(String line) {
         Pattern p = Pattern.compile("[.?!]");
         Matcher matcher = p.matcher(line);
         int lastEndSentence = 0;
         while (matcher.find()) {
-            lastEndSentence=matcher.end() - 1;
+            lastEndSentence = matcher.end() - 1;
         }
         return lastEndSentence;
     }
 
-    /** Проверяет есть  значащие символы в строке после окончания последнего полного предложения
+    /**
+     * Проверяет есть  значащие символы в строке после окончания последнего полного предложения
      *
      * @param line анализируемая строка
      * @return true если есть значащие символы, иначе false
@@ -121,39 +112,18 @@ public class ThreadSource implements Callable<List<String>> {
     private boolean isNotEndedSentence(String line) {
         Pattern p = Pattern.compile("[.?!].+?[А-Яа-яA-Za-z0-9]");
         Matcher matcher = p.matcher(line);
-        if (matcher.find()) {
-            return true;
-        } else {
-            return false;
-        }
-
+        return matcher.find();
     }
 
     /**
-     * Записывает в файл path из List<String> sentenceWithWords
+     * Проверяет есть  значащие символы в строке
      *
-     * @param sentenceWithWords List<String> который необходимо записать
-     * @param path путь к файлу, в который надо записать
-     * @throws FileNotFoundException если путь не найден или файл занят
+     * @param line анализируемая строка
+     * @return true если есть значащие символы, иначе false
      */
-    private static void writeToFile (List<String> sentenceWithWords, String path) throws FileNotFoundException {
-        File fileobj = new File("path");
-        if (fileobj.exists() && fileobj.delete()) {
-            throw new FileNotFoundException();
-
-        }
-        try (FileOutputStream fileOutputStream = new FileOutputStream(path)) {
-
-            for (String el : sentenceWithWords ){
-                byte[] buffer = (el+"\n").getBytes();
-                fileOutputStream.write(buffer, 0, buffer.length);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Путь не найден");
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("Что то пошло не так при записи в файл, возможно файл занят");
-            e.printStackTrace();
-        }
+    private boolean hasWords(String line) {
+        Pattern p = Pattern.compile("[А-Яа-яA-Za-z]");
+        Matcher matcher = p.matcher(line);
+        return matcher.find();
     }
 }
