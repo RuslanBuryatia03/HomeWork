1c4359547d8eecf5de1c0f8204b32f917756a030
KhankhasaevRV <rv2100@mail.ru>
2019-01-30 16:04:13 +0300

    Homework22 исправил по замечаниям Задание 3 опциональное.

--- src/ru/innopolis/homework22/Main.java
+++ src/ru/innopolis/homework22/Main.java
@@ -1,22 +1,19 @@
 package ru.innopolis.homework22;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
-//        MathBox mb = new MathBox(null);
-        Float[] arr = {9223372f , 9223385f, 92233720f, 8f, 3f, 2f};   //  9223372036854775807
-        MathBox mb = new MathBox(arr);
-        System.out.println(arr[1].getClass().getName());
-//        switch (arr[1].getClass().getName())
 
-        Number ddj = mb.summator(arr[0].getClass().getName());
-        System.out.println(ddj);
-        mb.splitter(190f, arr[0].getClass().getName());
-        System.out.println(mb.deleteEl(2f));
-//        System.out.println(mb.toString());
+        Double[] arr = {92.345 , 922., 922450., 8., 3., 2.};
+        MathBox mb = new MathBox<>(arr);
+        Number sumArray = mb.summator();
 
+        DecimalFormat myFormatter = new DecimalFormat("###,###.############");
+        String output = myFormatter.format(sumArray);
+
+        System.out.println("сумма " + output);
+        System.out.println(mb.splitter(190.));
+        System.out.println(mb.deleteElement(2.));
     }
 }
